76f0125315280bbf008635905619867b71aed224
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-01 15:12:14 -0500

    fix: Moved code to proper location

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+# /usr/bin/env python3
 
+
+# Modulus function
+def mod(x, n):
+    if not isinstance(n, int) or not isinstance(x, int):
+        raise ValueError("Must be an integer")
+    if n == 0:
+        raise ValueError("n cannot be zero")
+    value = x % n
+    return value
+
+
+# Prime function
+def prime(x):
+    if not isinstance(x, int):
+        raise ValueError("Must be an integer")
+    if x < 2:
+        return False
+    if x == 2:
+        return True
+    for i in range(3, x - 1):
+        if x % i == 0:
+            return False
+    return True
