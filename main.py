514c6626f78ed1eda28b6a6db61ab46778597288
SinTan1729 <sayantan.santra689@gmail.com>
2026-04-01 16:00:45 -0500

    fix: Working decompose function

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python3
 
 # Prime checking function
-def is_prime(x: int) -> bool:
-    if x < 2:
+def is_prime(n: int) -> bool:
+    if n < 2:
         return False
-    if x == 2:
+    if n == 2:
         return True
-    if x % 2 == 0:
+    if n % 2 == 0:
         return False
-    for i in range(3, int(pow(x, 0.5)) + 1, 2):
-        if x % i == 0:
+    for i in range(3, int(pow(n, 0.5)) + 1, 2):
+        if n % i == 0:
             return False
     return True
 
 
+def decompose(n: int) -> dict[int, int]:
+    if n < 0:
+        raise ValueError
+
+    def aux(i: int) -> dict[int, int]:
+        di = decompose(int(n / i))
+        j = di.get(i, 0) + 1
+        di[i] = j
+        return di
+
+    if n % 2 == 0:
+        return aux(2)
+    for i in [2] + list(range(3, int(pow(n, 0.5)) + 1, 2)):
+        if n % i == 0:
+            return aux(i)
+    return {n: 1}
+
+
 def main() -> None:
-    count = 0
-    for i in range(1, 100):
-        if is_prime(i):
-            print(i)
-            count += 1
-    print("Count:", count)
+    print(decompose(7 * 7 * 2 * 2 * 2 * 5 * 9))
 
 
 if __name__ == "__main__":
